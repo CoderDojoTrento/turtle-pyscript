f30e8f350b6905cca56ddc5711c9e9266f3ca3d0
David Leoni <noreply@davidleoni.it>
2025-03-22 19:15:24 +0100

    fixes test for sanitized filenames id #9

--- test/uitest_bad_filenames.py
+++ test/uitest_bad_filenames.py
@@ -7,13 +7,13 @@ screen = Screen()
 img1 = r"test/img/a bad %'thing'.gif"
 
 # already escaped, works
-#img1 = r"/test/img/a%20bad%20%27thing%27.gif"
+#img1 = r"test/img/a%20bad%20%27thing%27.gif"
 
 # not escaped, doesn't work 
-#screen.bgpic(r"/test/img/bg % 'ruins' ò.gif")
+#screen.bgpic(r"test/img/bg % 'ruins' ò.gif")
 
 # already escaped, works
-screen.bgpic(r"/test/img/bg%20%25%20%27ruins%27%20ò.gif")
+screen.bgpic(r"test/img/bg%20%25%20%27ruins%27%20ò.gif")
 
 
 screen.register_shape(img1)
