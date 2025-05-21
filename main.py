3929bb596efe99c451f57aa5853350d4f3610cc8
David Leoni <noreply@davidleoni.it>
2025-05-21 10:06:29 +0200

    Cleaned main.py

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
+import turtleps
 
+# Write here some Python code and make your fabulous game!
 
-
-from turtleps import *
-
-screen = Screen()
-screen.bgpic("img/bg-seaside-2.gif")
-
-iarc =  "img/ch-archeologist-e.gif"  # looks east
-screen.register_shape(iarc)
-
-await ge_init()
-
-ada = Sprite()     
-ada.goto(-200,0) 
-ada.shape(iarc)    
-await ada.slide(0,0, 2) 
-await ada.say("Hello!", 5)
